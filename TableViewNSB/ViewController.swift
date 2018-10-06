2223273e19630bc6a48695380de304ff9383ac16
adeluleye <adelu4real@gmail.com>
2018-10-06 23:50:45 +0100

    set the tableView as delegate and datasource, implement the required functions

--- TableViewNSB/ViewController.swift
+++ TableViewNSB/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let tableView: UITableView = {
         let tableView = UITableView()
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
         tableView.allowsSelection = false
         return tableView
     }()
+    
+    let bandCellId = "bandCellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +32,44 @@ class ViewController: UIViewController {
     }
     
     func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
         view.addSubview(tableView)
         tableView.setAnchor(top: view.topAnchor, topPad: 0, bottom: view.bottomAnchor, bottomPad: 0, left: view.leftAnchor, leftPad: 0, right: view.rightAnchor, rightPad: 0, height: 0, width: 0)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: bandCellId, for: indexPath) as! BandCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
+
+class BandCell: UITableViewCell {
+    
+    let cellView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.setCellShadow()
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
